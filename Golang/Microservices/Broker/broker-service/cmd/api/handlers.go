269245c4dcfad71464c4a07175c98ec8e3b1f7ea
413831 <alepleins3pp@gmail.com>
2026-03-25 20:19:44 -0300

    added json utils helper

--- Golang/Microservices/Broker/broker-service/cmd/api/handlers.go
+++ Golang/Microservices/Broker/broker-service/cmd/api/handlers.go
@@ -1,15 +1,6 @@
 package main
 
-import (
-	"encoding/json"
-	"net/http"
-)
-
-type jsonResponse struct {
-	Error   bool   `json:"error"`
-	Message string `json:"message"`
-	Data    any    `json:"data,omitempty"`
-}
+import "net/http"
 
 func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
 	payload := jsonResponse{
@@ -17,8 +8,5 @@ func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
 		Message: "Hit from broker",
 	}
 
-	out, _ := json.MarshalIndent(payload, "", "\t")
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusAccepted)
-	w.Write(out)
+	_ = app.writeJSON(w, http.StatusOK, payload)
 }
