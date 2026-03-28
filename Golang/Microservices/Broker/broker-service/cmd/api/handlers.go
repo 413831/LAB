abc073e035630d12f1bb7cc158859ebc168aae87
413831 <alepleins3pp@gmail.com>
2026-03-28 13:36:56 -0300

    added frontend auth call

--- Golang/Microservices/Broker/broker-service/cmd/api/handlers.go
+++ Golang/Microservices/Broker/broker-service/cmd/api/handlers.go
@@ -47,7 +47,7 @@ func (app *Config) HandleSubmission(w http.ResponseWriter, r *http.Request) {
 func (app *Config) authenticate(w http.ResponseWriter, a AuthPayload) {
 	jsonData, _ := json.MarshalIndent(a, "", "\t")
 
-	request, err := http.NewRequest("POST", "http://authentication_service/authenticate", bytes.NewBuffer(jsonData))
+	request, err := http.NewRequest("POST", "http://authentication-service/authenticate", bytes.NewBuffer(jsonData))
 	if err != nil {
 		app.errorJSON(w, err)
 		return
