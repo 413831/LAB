549d8f16e970841806b53c7d4b9e07854100cafd
413831 <alepleins3pp@gmail.com>
2026-03-30 20:23:33 -0300

    added logger docker config

--- Golang/Microservices/Broker/logger-service/cmd/api/main.go
+++ Golang/Microservices/Broker/logger-service/cmd/api/main.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
+	"log-service/data"
+	"net/http"
+	"time"
 
 	"go.mongodb.org/mongo-driver/v2/mongo"
 	"go.mongodb.org/mongo-driver/v2/mongo/options"
@@ -17,6 +22,7 @@ const (
 var client *mongo.Client
 
 type Config struct {
+	Models data.Models
 }
 
 func main() {
@@ -27,6 +33,44 @@ func main() {
 
 	client = mongoClient
 
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+
+	// close connection
+	defer func() {
+		if err = client.Disconnect(ctx); err != nil {
+			log.Panic(err)
+		}
+	}()
+
+	app := Config{
+		Models: data.New(client),
+	}
+
+	//go app.serve()
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%s", webPort),
+		Handler: app.routes(),
+	}
+
+	log.Printf("Starting logger service on port %s", webPort)
+
+	if err := srv.ListenAndServe(); err != nil {
+		log.Panic(err)
+	}
+}
+
+func (app *Config) serve() {
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%s", webPort),
+		Handler: app.routes(),
+	}
+
+	log.Printf("Starting logger service on port %s", webPort)
+
+	if err := srv.ListenAndServe(); err != nil {
+		log.Panic(err)
+	}
 }
 
 func connectToMongo() (*mongo.Client, error) {
